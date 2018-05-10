62369988144e0ef59b0506ab90cb2a2872c19ce4
Mafwo <mafwolikelenge@gmail.com>
2018-05-10 13:16:12 +0200

    complete routed forms

---
+++ imports/ui/invisNavBar/NavBar1.jsx
@@ -0,0 +1,47 @@
+import {Meteor} from 'meteor/meteor';
+import React, { Component } from 'react';
+// import './App.css';
+import route from '/imports/routing/router.js';
+// route('/', Home);
+
+
+export default class NavBar1 extends Component { 
+  goToHome = () => {
+    route.go('/') // pathDef, params, queryParams
+  }
+  goToSignUp = () => {
+    route.go('/SignUp') // pathDef, params, queryParams
+}
+
+  goToUsign = () => {
+    route.go('/UserSign') // pathDef, params, queryParams
+}
+goTolog = () => {
+  route.go('/LogIn') // pathDef, params, queryParams
+}
+
+  render() {
+    return ( 
+     <div> 
+     <nav className="navbar navbar-inverse navbar-fixed-top trans-nav">
+       <a className="navbar-brand" href="#">
+         <img src="logo2.png" className="d-inline-block align-top" alt=""/>
+       </a>
+       <ul className="nav justify-Dropdowncontent-end">
+    <li className="nav-item dropdown">
+     <a className="nav-link dropdown-toggle"  href="" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          SignUp </a>        
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="/SignUp">Artist</a>
+          <a className="dropdown-item" href="/UserSign">User</a> 
+          </div>
+      </li>
+  <li className="nav-item">
+    <a className="nav-link active" href="/LogIn">LogIn</a>
+  </li>
+  </ul>
+     </nav>
+   </div>
+    );
+  } 
+}
