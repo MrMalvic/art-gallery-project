dc9a085477dda83d79d8ebe7c49ab0ed533ee747
MrMalvic <mutalevictor6@gmail.com>
2018-05-10 11:32:35 +0200

    styled artist signup

--- imports/ui/invisNavBar/ArtSignUp.jsx
+++ imports/ui/invisNavBar/ArtSignUp.jsx
@@ -1,4 +1,4 @@
-import {Meteor} from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import route from '/imports/routing/router.js';
 import NavBar1 from './NavBar1.jsx';
@@ -6,62 +6,70 @@ import NavBar1 from './NavBar1.jsx';
 
 
 export default class ArtSignUp extends Component {
-    // goToHome = () => {
-    //     route.go('/') // pathDef, params, queryParams
-    
-    // }
+  goToHome = () => {
+      route.go('/') // pathDef, params, queryParams
+
+  }
+
+  
   render() {
-    
+
     return (
       <div className="App">
-      <NavBar1/>
-      <br /><br />
-        <h1>Artist Registration</h1>< br /><br />
-        <form onSubmit = {this.handleSubmit}>
-  <div className="form-row">
-    <div className="form-group col-md-6">
-      <label for="inputName">Full Names</label>
-      <input type="text" class="form-control" id="inputName" placeholder="Full Names"/>
-    </div>
-    <div className="form-group col-md-6">
-      <label for="inputEmail">Email</label>
-      <input type="email" class="form-control" id="inputEmail" placeholder="Email"/>
-      </div>
-    <div className="form-group col-md-6">
-      <label for="inputPassword">Password</label>
-      <input type="password" class="form-control" id="inputPassword" placeholder="Password"/>
-    </div>
-    <div className="form-group col-md-6">
-      <label for="inputConfirm">Confirm Password</label>
-      <input type="password" class="form-control" id="inputConfirm" placeholder="Confirm Password"/>
-    </div>
-  </div>
-    <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">Location</label>
-      <input type="text" class="form-control" id="inputCity"placeholder="Your Location"/>
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputState">Category</label>
-      <select id="inputCategory" class="form-control">
-        <option selected>Choose Category</option>
+        <div className="mask3">
+          <img src="img1.jpg" className="artist-signup-img" />
+        </div>
+        <NavBar1 />
+        <div>
+        <form onSubmit={this.handleSubmit}  className="artist-signup-form container-fluid">
+        <h1 className="artist-reg">Artist Registration</h1>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label for="inputName">Full Names</label>
+              <input type="text" className="form-control" id="inputName" placeholder="Full Names" />
+            </div>
+            <div className="form-group col-md-6">
+              <label for="inputEmail">Email</label>
+              <input type="email" className="form-control" id="inputEmail" placeholder="Email" />
+            </div>
+            <div className="form-group col-md-6">
+              <label for="inputPassword">Password</label>
+              <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
+            </div>
+            <div className="form-group col-md-6">
+              <label for="inputConfirm">Confirm Password</label>
+              <input type="password" className="form-control" id="inputConfirm" placeholder="Confirm Password" />
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label for="inputCity">Location</label>
+              <input type="text" className="form-control" id="inputCity" placeholder="Your Location" />
+            </div>
+            <div className="form-group col-md-6">
+              <label for="inputState">Category</label>
+              <select id="inputCategory" className="form-control">
+                <option selected>Choose Category</option>
                 <option>Crafts</option>
                 <option>Drawings</option>
                 <option>Paintings</option>
                 <option>Sculpures</option>
-      </select>
-    </div>
-    </div>
-    <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck"/>
-      <label class="form-check-label" for="gridCheck">
-       I agree to Elite's Ts&Cs
+              </select>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="form-check">
+              
+              <label className="form-check-label" for="gridCheck">
+              <input className="form-check-input" type="checkbox" id="gridCheck" />
+                I agree to Elite's Ts&Cs
       </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign Up</button>
-</form>
+            </div>
+            </div>
+          
+          <button type="submit" class="btn btn-primary">Sign Up</button>
+        </form>
+        </div>
         {/* <form onSubmit = {this.handleSubmit}>
             <input type="text" placeholder="Full Names"/> <br/>
             <input type="number" placeholder="age"/><br />
@@ -77,7 +85,7 @@ export default class ArtSignUp extends Component {
       </div>
     );
   }
-  
+
 }
 
 
