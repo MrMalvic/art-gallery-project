8dca10b922a3e9768b221d73bbac6a9e91d60e9a
MrMalvic <mutalevictor6@gmail.com>
2018-05-15 12:01:31 +0200

    styled categories and fixed errors

--- imports/ui/Craft.jsx
+++ imports/ui/Craft.jsx
@@ -1,135 +1,101 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
+import { Navbar2 } from '/imports/ui/Navbar2.jsx';
+import { Footer } from '/imports/ui/Footer.jsx';
 
 
 
 
+export class Craft extends Component {
 
-export class Craft extends Component{
+  goToCraft = () => {
+    route.go('/craft');
+  }
 
-    goToCraft = () => {
-        route.go('/craft');
-      }
+  goToPops = () => {
+    route.go('/Pops');
+  }
 
-      goToPops = () => {
-          route.go('/Pops');
-        }
-
-    render(){
-        return(
+  render() {
+    return (
 
       <div className="App">
-         <nav class="navbar navbar-expand-lg navbar-light bg-purple">
-           <a class="navbar-brand" href="/">Elite Art</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-               <span class="navbar-toggler-icon"></span>
-                </button>
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-             <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/about">About</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Categories
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="/painting">Painting</a>
-              <a class="dropdown-item" href="/sculpture">Sculpture</a>
-              <a class="dropdown-item" href="/drawing">Drawing</a>
-              <a class="dropdown-item" href="/craft">Craft</a>
-            </div>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Discovery
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="/Pops">Popular Artists</a>
-              <a class="dropdown-item" href="#">New Artists</a>
-              <a class="dropdown-item" href="#">Other Artists</a>
-            </div>
-          </li>
-          </ul>
-      </div>
-    </nav>
-                      <h1>CRAFTS</h1>
-    <div class="row">
-      <div class="col-md-4">
-    <img class="card-img-top" src="craft/let.jpeg" width="200%" height="80%" alt="Card image cap"></img>
-    <br>
-    </br>
-    <a class="btn btn-success" href="/low1"><h5>Buy</h5></a>
-           </div>
-        <div class="col-md-4">
-     <img class="card-img-top" src="craft/awes.jpeg" width="200%" height="80%" alt="Card image cap"></img>
-     <br>
-     </br>
-     <a class="btn btn-success" href="/low2"><h5>Buy</h5></a>
-      </div>
-      <div class="col-md-4">
-    <img class="card-img-top" src="craft/super.jpeg" width="200%" height="80%" alt="Card image cap"></img>
-    <br>
-    </br>
-    <a class="btn btn-success" href="/low3"><h5>Buy</h5></a>
-           </div>
-           </div>
-           <div class="row">
-        <div class="col-md-4">
-        <img class="card-img-top" src="craft/less.jpeg" width="200%" height="80%" alt="Card image cap"></img>
-        <br>
-        </br>
-        <a class="btn btn-success" href="/low4"><h5>Buy</h5></a>
-      </div>
-      <div class="col-md-4">
-        <img class="card-img-top" src="craft/inde.jpeg" width="20%" height="80%" alt="Card image cap"></img>
-        <br>
-        </br>
-        <a class="btn btn-success" href="/low5"><h5>Buy</h5></a>
-      </div>
-      <div class="col-md-4">
-        <img class="card-img-top" src="craft/toot.jpeg"  width="200%" height="80%" alt="Card image cap"></img>
-        <br></br>
-        <a class="btn btn-success" href="/low6"><h5>Buy</h5></a>
-      </div>
-      </div>
-      <div class="row">
-      <div class="col-md-4">
-        <img class="card-img-top" src="craft/great.jpeg" width="200%" height="80%" alt="Card image cap"></img>
-        <br></br>
-        <a class="btn btn-success" href="/low7"><h5>Buy</h5></a>
-      </div>
-      <div class="col-md-4">
-        <img class="card-img-top" src="craft/impo.jpeg"  width="200%" height="80%" alt="Card image cap"></img>
-        <br></br>
-        <a class="btn btn-success" href="/low8"><h5>Buy</h5></a>
-      </div>
-      <div class="col-md-4">
-    <img class="card-img-top" src="craft/more.jpeg" width="200%" height="80%" alt="Card image cap"></img>
-    <br></br>
-    <a class="btn btn-success" href="/low9"><h5>Buy</h5></a>
-      </div>
-      </div>
-           <div class="row">
-        <div class="col-md-4">
-        <img class="card-img-top" src="craft/cool.jpeg" width="200%" height="80%" alt="Card image cap"></img>
-        <br></br>
-        <a class="btn btn-success" href="/low10"><h5>Buy</h5></a>
-      </div>
-      <div class="col-md-4">
-        <img class="card-img-top" src="craft/sir.jpeg" width="200%" height="80%" alt="Card image cap"></img>
-        <br></br>
-        <a class="btn btn-success" href="/low11"><h5>Buy</h5></a>
-      </div>
-      <div class="col-md-4">
-        <img class="card-img-top" src="craft/eat.jpeg"  width="200%" height="80%" alt="Card image cap"></img>
-        <a class="btn btn-success" href="/clow12"><h5>Buy</h5></a>
+        <Navbar2 />
+        <h1>CRAFTS</h1>
+        <div className="row">
+          <div className="col-md-4">
+            <img className="card-img-top thumb" src="craft/let.jpeg" alt="Card image cap"></img>
+            <br>
+            </br>
+            <a className="btn btn-success" href="/low1"><h5>Buy</h5></a>
+          </div>
+          <div className="col-md-4">
+            <img className="card-img-top thumb" src="craft/awes.jpeg" alt="Card image cap"></img>
+            <br>
+            </br>
+            <a className="btn btn-success" href="/low2"><h5>Buy</h5></a>
+          </div>
+          <div className="col-md-4">
+            <img className="card-img-top thumb" src="craft/super.jpeg" alt="Card image cap"></img>
+            <br>
+            </br>
+            <a className="btn btn-success" href="/low3"><h5>Buy</h5></a>
+          </div>
         </div>
+        <div className="row">
+          <div className="col-md-4">
+            <img className="card-img-top thumb" src="craft/less.jpeg" alt="Card image cap"></img>
+            <br>
+            </br>
+            <a className="btn btn-success" href="/low4"><h5>Buy</h5></a>
+          </div>
+          <div className="col-md-4">
+            <img className="card-img-top thumb" src="craft/inde.jpeg" width="20%" height="80%" alt="Card image cap"></img>
+            <br>
+            </br>
+            <a className="btn btn-success" href="/low5"><h5>Buy</h5></a>
+          </div>
+          <div className="col-md-4">
+            <img className="card-img-top thumb" src="craft/toot.jpeg" alt="Card image cap"></img>
+            <br></br>
+            <a className="btn btn-success" href="/low6"><h5>Buy</h5></a>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <img className="card-img-top thumb" src="craft/great.jpeg" alt="Card image cap"></img>
+            <br></br>
+            <a className="btn btn-success" href="/low7"><h5>Buy</h5></a>
+          </div>
+          <div className="col-md-4">
+            <img className="card-img-top thumb" src="craft/impo.jpeg" alt="Card image cap"></img>
+            <br></br>
+            <a className="btn btn-success" href="/low8"><h5>Buy</h5></a>
+          </div>
+          <div className="col-md-4">
+            <img className="card-img-top thumb" src="craft/more.jpeg" alt="Card image cap"></img>
+            <br></br>
+            <a className="btn btn-success" href="/low9"><h5>Buy</h5></a>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <img className="card-img-top thumb" src="craft/cool.jpeg" alt="Card image cap"></img>
+            <br></br>
+            <a className="btn btn-success" href="/low10"><h5>Buy</h5></a>
+          </div>
+          <div className="col-md-4">
+            <img className="card-img-top thumb" src="craft/sir.jpeg" alt="Card image cap"></img>
+            <br></br>
+            <a className="btn btn-success" href="/low11"><h5>Buy</h5></a>
+          </div>
+          <div className="col-md-4">
+            <img className="card-img-top thumb" src="craft/eat.jpeg" alt="Card image cap"></img>
+            <a className="btn btn-success" href="/clow12"><h5>Buy</h5></a>
+          </div>
+        </div>
+        <Footer />
       </div>
-      </div>
-        );
-    }
+    );
+  }
 };
