92bb99b905299def294bca1d40794b6fd7acf251
Mafwo <mafwolikelenge@gmail.com>
2018-05-15 12:46:01 +0200

    ...

--- imports/ui/invisNavBar/ArtSignUp.jsx
+++ imports/ui/invisNavBar/ArtSignUp.jsx
@@ -4,44 +4,47 @@ import route from '/imports/routing/router.js';
 import NavBar1 from './NavBar1.jsx';
 
 export class ArtSignUp extends Component {
- 
+
+     
   render() {
 
     return (
+      
       <div className="App">
         <div className="mask3">
           <img src="img1.jpg" className="artist-signup-img" />
         </div>
         <NavBar1 />
         <div>
-        <form onSubmit={this.getUserData}  className="artist-signup-form container-fluid">
-        <h1 className="artist-reg">Artist Registration</h1>
+        <form onSubmit= {this.getUserData}  className="artist-signup-form container-fluid needs-validation">
+        <h1 className="artist-reg">Artist Registration</h1> 
+        
           <div className="form-row">
             <div className="form-group col-md-6">
               <label htmlFor="inputName">Full Names</label>
-              <input type="text" className="form-control" id="inputName" name="name" placeholder="Full Names" />
+              <input type="text" className="form-control" name="name" id="inputName" placeholder="Full Names" required />
             </div>
             <div className="form-group col-md-6">
               <label htmlFor="inputEmail">Email</label>
-              <input type="email" className="form-control" id="inputEmail" name="email" placeholder="Email" />
+              <input type="email" className="form-control" name="email"id="inputEmail" placeholder="Email" required/>
             </div>
             <div className="form-group col-md-6">
               <label htmlFor="inputPassword">Password</label>
-              <input type="password" className="form-control" id="inputPassword" name="password" placeholder="Password" />
+              <input type="password" className="form-control" name="password" id="inputPassword" placeholder="Password" required />
             </div>
             <div className="form-group col-md-6">
               <label htmlFor="inputConfirm">Confirm Password</label>
-              <input type="password" className="form-control" id="inputConfirm" name="confirmPassword" placeholder="Confirm Password" />
+              <input type="password" className="form-control" name="password2" id="inputConfirm" placeholder="Confirm Password"required />
             </div>
           </div>
           <div className="form-row">
             <div className="form-group col-md-6">
               <label htmlFor="inputCity">Location</label>
-              <input type="text" className="form-control" id="inputCity" name="location" placeholder="Your Location" />
+              <input type="text" className="form-control" name="location" id="inputCity" placeholder="Your Location" />
             </div>
             <div className="form-group col-md-6">
               <label htmlFor="inputState">Category</label>
-              <select id="inputCategory" className="form-control">
+              <select name="category" id="inputCategory" className="form-control">
                 <option>Choose Category</option>
                 <option>Crafts</option>
                 <option>Drawings</option>
@@ -53,8 +56,9 @@ export class ArtSignUp extends Component {
           <div className="form-group">
             <div className="form-check">
               
-              <label className="form-check-label" htmlFor="gridCheck">
+              
               <input className="form-check-input" type="checkbox" id="gridCheck" />
+              <label className="form-check-label" htmlFor="gridCheck">
                 I agree to Elite's Ts&Cs
       </label>
             </div>
@@ -62,19 +66,8 @@ export class ArtSignUp extends Component {
           
           <button type="submit" className="btn btn-primary">Sign Up</button>
         </form>
-        </div>
-        {/* <form onSubmit = {this.handleSubmit}>
-            <input type="text" placeholder="Full Names"/> <br/>
-            <input type="number" placeholder="age"/><br />
-            <input type="text" placeholder="Email"/><br />
-            <
-            <input type="text"placeholder="Art Category" /> <br />
-            <input type="text" placeholder="Location"/><br /> 
-            <input type="password" placeholder="Password"/><br />
-            <input type="password" placeholder="Confirm Password"/><br /> 
-            
-            <button type="submit">Sign Up</button>            
-        </form>  */}
+        </div>      
+        <Footer/>
       </div>
     );
   }
