d3055fa42a5e5ee382884ff0d02c2adaef567f14
MrMalvic <mutalevictor6@gmail.com>
2018-05-16 14:07:56 +0200

    implemented make-shift navbar state

--- imports/ui/invisNavBar/NavBar1.jsx
+++ imports/ui/invisNavBar/NavBar1.jsx
@@ -1,4 +1,4 @@
-import {Meteor} from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 // import './App.css';
 import route from '/imports/routing/router.js';
@@ -6,27 +6,26 @@ import route from '/imports/routing/router.js';
 import $ from 'jquery';
 
 
-export default class NavBar1 extends Component { 
+export default class NavBar1 extends Component {
 
-  componentDidMount(){
-    $(document).click(() =>{
-      if(Meteor.user()){
-        $('#navbarDropdown').hide();
-        $('#logIn').hide();
-        $('#logOut').show();
+  componentDidMount() {
+    $(document).ready(() => {
+      if (Meteor.user()) {
+        $('#nav1').hide();
+        $('#nav2').show();
       }
-      else{
-        $('#navbarDropdown').show();
-        $('#logIn').show();
-        $('#logOut').hide();
+      else {
+        $('#nav1').show();
+        $('#nav2').hide();
       }
     })
   }
 
-  logUserOut = (e) =>{
+  logUserOut = (e) => {
     e.preventDefault();
     console.log('you are now logged out');
     Meteor.logout();
+    window.location.reload()
   }
 
   goToHome = () => {
@@ -34,42 +33,47 @@ export default class NavBar1 extends Component {
   }
   goToSignUp = () => {
     route.go('/SignUp') // pathDef, params, queryParams
-}
+  }
 
   goToUsign = () => {
     route.go('/UserSign') // pathDef, params, queryParams
-}
-goTolog = () => {
-  route.go('/LogIn') // pathDef, params, queryParams
-}
+  }
+  goTolog = () => {
+    route.go('/LogIn') // pathDef, params, queryParams
+  }
 
   render() {
-    return ( 
-     <div> 
-     <nav className="navbar navbar-inverse navbar-fixed-top trans-nav">
-       <a className="navbar-brand" href="/">
-         <img src="logo2.png" className="d-inline-block align-top" alt=""/>
-       </a>
-       <ul className="nav justify-Dropdowncontent-end">
-    <li className="nav-item dropdown">
-
-
-     <a className="nav-link dropdown-toggle"  href="" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          SignUp </a>        
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item"  href="/SignUp">Artist</a>
-          <a className="dropdown-item"  href="/UserSign">User</a> 
-          </div>
-      </li>
-  <li className="nav-item">
-    <a className="nav-link active" id="logIn" href="/LogIn">LogIn</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link active" id="logOut" href="#" onClick={this.logUserOut}>LogOut</a>
-  </li>
-  </ul>
-     </nav>
-   </div>
+    return (
+      <div>
+        <nav className="navbar navbar-inverse navbar-fixed-top trans-nav" id="nav1">
+          <a className="navbar-brand" href="/">
+            <img src="logo2.png" className="d-inline-block align-top" alt="" />
+          </a>
+          <ul className="nav justify-Dropdowncontent-end">
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                SignUp </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="/SignUp">Artist</a>
+                <a className="dropdown-item" href="/UserSign">User</a>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" id="logIn" href="/LogIn">LogIn</a>
+            </li>
+          </ul>
+        </nav>
+        <nav className="navbar navbar-inverse navbar-fixed-top trans-nav" id="nav2">
+          <a className="navbar-brand" href="/">
+            <img src="logo2.png" className="d-inline-block align-top" alt="" />
+          </a>
+          <ul className="nav justify-Dropdowncontent-end">
+            <li className="nav-item">
+              <a className="nav-link active" id="logOut" href="#" onClick={this.logUserOut}>LogOut</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     );
-  } 
+  }
 }
