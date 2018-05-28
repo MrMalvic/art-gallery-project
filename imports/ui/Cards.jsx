1afc96ef443b48a707d51c256487b6403c45ea2f
MrMalvic <mutalevictor6@gmail.com>
2018-05-28 13:56:16 +0200

    added hover items

--- imports/ui/Cards.jsx
+++ imports/ui/Cards.jsx
@@ -28,26 +28,36 @@ export class Cards extends Component {
 
           <div className=" hover-over-imgs">
             <img className="thumbnail" src="painting/ab.jpg" />
-            <a className="on-hover-details" href="/Buy1"></a>
-          </div>
+            <div className=" on-hover-details" onClick={this.linkToBuy}>
+              <a href="#"><i className="fas fa-heart fa-2x hover-item-one" title="i like this"></i></a>
+              <a className="artist-links hover-item-two" title="artist" href="/scul1"><img src="a.jpg" className="small-img pad2" alt="artist" />Artist</a>
+            </div>          </div>
 
           <div className=" hover-over-imgs">
             <img className="thumbnail" src="painting/ac.jpeg" />
-            <a className="on-hover-details" href="/Buy1"></a>
-          </div>
+            <div className=" on-hover-details" onClick={this.linkToBuy}>
+              <a href="#"><i className="fas fa-heart fa-2x hover-item-one" title="i like this"></i></a>
+              <a className="artist-links hover-item-two" title="artist" href="/scul1"><img src="a.jpg" className="small-img pad2" alt="artist" />Artist</a>
+            </div>          </div>
 
           <div className=" hover-over-imgs">
             <img className="thumbnail" src="painting/ad.jpg" />
-            <a className="on-hover-details" href="/Buy1"></a>
-          </div>
+            <div className=" on-hover-details" onClick={this.linkToBuy}>
+              <a href="#"><i className="fas fa-heart fa-2x hover-item-one" title="i like this"></i></a>
+              <a className="artist-links hover-item-two" title="artist" href="/scul1"><img src="a.jpg" className="small-img pad2" alt="artist" />Artist</a>
+            </div>          </div>
           <div className=" hover-over-imgs">
             <img className="thumbnail" src="painting/ae.jpg" />
-            <a className="on-hover-details" href="/Buy1"></a>
-          </div>
+            <div className=" on-hover-details" onClick={this.linkToBuy}>
+              <a href="#"><i className="fas fa-heart fa-2x hover-item-one" title="i like this"></i></a>
+              <a className="artist-links hover-item-two" title="artist" href="/scul1"><img src="a.jpg" className="small-img pad2" alt="artist" />Artist</a>
+            </div>          </div>
           <div className=" hover-over-imgs">
             <img className="thumbnail" src="painting/af.jpg" />
-            <a className="on-hover-details" href="/Buy1"></a>
-          </div>
+            <div className=" on-hover-details" onClick={this.linkToBuy}>
+              <a href="#"><i className="fas fa-heart fa-2x hover-item-one" title="i like this"></i></a>
+              <a className="artist-links hover-item-two" title="artist" href="/scul1"><img src="a.jpg" className="small-img pad2" alt="artist" />Artist</a>
+            </div>          </div>
         </div>
       </div>
 
